8fddde3973114cf5b5716c74c95d14df336c97b5
Kyle Graehl <kgraehl@gmail.com>
2016-04-24 18:24:55 -0700

    upnp getting mappings now

--- js/upnp.js
+++ js/upnp.js
@@ -1,65 +1,199 @@
-// ssdp
+// inspiration from https://github.com/indutny/node-nat-upnp
 (function() {
+    function flatParseNode(node) {
+        var d = {}
+        for (var i=0; i<node.children.length; i++) {
+            var c = node.children[i]
+            if (c.children.length == 0) {
+                d[c.tagName] = c.innerHTML
+            }
+        }
+        return d
+    }
+    
     function UPNP(opts) {
         this.ssdp = new SSDP({client:this})
         this.ssdp.addEventListener('device',this.onDevice.bind(this))
+        this.ssdp.addEventListener('stop',this.onSearchStop.bind(this))
         this.ssdp.search() // stop searching after a bit.
         this.devices = []
+        this.desiredServices = [
+            'urn:schemas-upnp-org:service:WANIPConnection:1',
+            'urn:schemas-upnp-org:service:WANPPPConnection:1'
+        ]
+        this.validGateway = null
     }
     UPNP.prototype = {
+        onSearchStop: function(info) {
+            this.getIP( function() {
+                this.getMappings( function(mappings) {
+                    console.log('got current mappings',mappings)
+                })
+            }.bind(this))
+        },
         onDevice: function(info) {
             console.clog(L.SSDP, 'found an internet gateway device',info)
             var device = new GatewayDevice(info)
-            this.devices.push( device )
+            device.getDescription( function() {
+                this.devices.push( device )
+            }.bind(this) )
+        },
+        getWANServiceInfo: function() {
+            var infos = []
+            for (var i=0; i<this.devices.length; i++) {
+                var services = this.devices[i].getService(this.desiredServices)
+                if (services.length > 0) {
+                    for (var j=0; j<services.length; j++) {
+                        infos.push( {service:services[j],
+                                     device:this.devices[i]} )
+                    }
+                }
+            }
+            console.log('found WAN services',infos)
+            return infos
+        },
+        getMappings: function(callback) {
+            if (! this.validGateway) {
+                callback()
+            } else {
+                var info = this.validGateway
+                var idx = 0
+                var mappings = []
+
+                function oneResult(evt) {
+                    if (evt.target.code == 200) {
+                        var resp = evt.target.responseXML.querySelector("GetGenericPortMappingEntryResponse")
+                        var mapping = flatParseNode(resp)
+                        mappings.push(mapping)
+                        getOne()
+                    } else {
+                        callback(mappings)
+                    }
+                }
+
+                function getOne() {
+                    info.device.runService(info.service, 'GetGenericPortMappingEntry', [['NewPortMappingIndex',++idx]], oneResult)
+                }
+                getOne()
+            }
         },
         getIP: function(callback) {
-            if (this.devices.length == 0) {
-                callback({error:'no devices'})
-                return
+            var infos = this.getWANServiceInfo()
+            var foundIP = null
+            var returned = 0
+
+            function oneResult(info, evt) {
+                var doc = evt.target.responseXML
+                var ipelt = doc.documentElement.querySelector('NewExternalIPAddress')
+                var ip = ipelt ? ipelt.innerHTML : null
+
+                returned++
+                info.device.externalIP = ip
+                if (ip) {
+                    foundIP = ip
+                    this.validGateway = info
+                }
+                
+                if (returned == infos.length) {
+                    callback(foundIP)
+                }
             }
-            var device = this.devices[0]
-            device.request('hello', callback)
+            
+            if (infos && infos.length > 0) {
+                for (var i=0; i<infos.length; i++) {
+                    var info = infos[i]
+                    info.device.runService(info.service,'GetExternalIPAddress',[],oneResult.bind(this, info))
+                }
+            }
         }
     }
     
     function GatewayDevice(info) {
         this.info = info
         this.description_url = info.headers.location
-        this.services = [
-            'urn:schemas-upnp-org:service:WANIPConnection:1',
-            'urn:schemas-upnp-org:service:WANPPPConnection:1'
-        ]
+        this.services = []
+        this.devices = []
+        this.attributes = null
+        this.externalIP = null
     }
     GatewayDevice.prototype = {
-        get: function(url, callback) {
+        runService: function(service, command, args, callback) {
             var xhr = new WSC.ChromeSocketXMLHttpRequest
-            console.clog(L.SSDP,'query',this.description_url)
-            xhr.open("GET",url)
+            var url = new URL(this.description_url).origin + service.controlURL
+            var body = '<?xml version="1.0"?>' +
+                '<s:Envelope ' +
+                'xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" ' +
+                's:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">' +
+                '<s:Body>' +
+                '<u:' + command + ' xmlns:u=' +
+                JSON.stringify(service.serviceType) + '>' +
+                args.map(function(args) {
+                    return '<' + args[0]+ '>' +
+                        (args[1] === undefined ? '' : args[1]) +
+                        '</' + args[0] + '>';
+                }).join('') +
+                '</u:' + command + '>' +
+                '</s:Body>' +
+                '</s:Envelope>';
+            //console.log('req body',body)
+            var payload = new TextEncoder('utf-8').encode(body).buffer
+            var headers = {
+                'content-type':'text/xml; charset="utf-8"',
+                'connection':'close',
+                'SOAPAction': JSON.stringify(service.serviceType) + '#' + command
+            }
+            for (var k in headers) {
+                xhr.setRequestHeader(k, headers[k])
+            }
+            xhr.open("POST",url)
+            xhr.responseType = 'xml'
+            xhr.send(payload)
             function onload(evt) {
                 if (evt.target.code == 200) {
-                    var response = new TextDecoder('utf-8').decode(evt.target.response)
-                    var parser = new DOMParser
-                    var parsed = parser.parseFromString(response, "text/xml")
-                    var services = parsed.documentElement.querySelectorAll("serviceList service")
-                    var devices = parsed.documentElement.querySelectorAll("deviceList device")
-                    debugger
                 }
+                callback()
             }
-            xhr.onload = xhr.onerror = xhr.ontimeout = onload
+            xhr.onload = xhr.onerror = xhr.ontimeout = callback
+        },
+        getDescription: function(callback) {
+            var xhr = new WSC.ChromeSocketXMLHttpRequest
+            console.clog(L.SSDP,'query',this.description_url)
+            xhr.open("GET",this.description_url)
+            xhr.responseType = 'xml'
+            function onload(evt) {
+                if (evt.target.code == 200) {
+                    var doc = evt.target.responseXML
+
+                    var devices = doc.documentElement.querySelectorAll('device')
+                    for (var i=0; i<devices.length; i++) {
+                        this.devices.push( flatParseNode(devices[i]) )
+                    }
+
+                    var services = doc.documentElement.querySelectorAll('service')
+                    for (var i=0; i<services.length; i++) {
+                        this.services.push( flatParseNode(services[i]) )
+                    }
+
+                }
+                console.log('got service info',this)
+                callback()
+            }
+            xhr.onload = xhr.onerror = xhr.ontimeout = onload.bind(this)
             xhr.send()
         },
-        get_service: function(name, callback) {
-            this.get(this.description_url, callback)
-        },
-        request: function(action, callback) {
-            this.get_service(name,callback)
+        getService: function(desired) {
+            var matches = this.services.filter( function(service) {
+                return desired.indexOf(service.serviceType) != -1
+            })
+            return matches
         }
     }
     
     function SSDP(opts) {
         this.client = opts.client
         this.multicast = '239.255.255.250'
-        this.port = 1900
+        this.ssdpPort = 1900
+        this.boundPort = null
         this.searchdevice = 'urn:schemas-upnp-org:device:InternetGatewayDevice:1'
         chrome.sockets.udp.onReceive.addListener( this.onReceive.bind(this) )
         chrome.sockets.udp.onReceiveError.addListener( this.onReceive.bind(this) )
@@ -123,17 +257,12 @@
             // stop searching, kill all sockets
             this.searching = false
             this.cleanup()
+            this.trigger('stop')
         },
         search: function(opts) {
             if (this.searching) { return }
-            setTimeout( this.stopsearch.bind(this), 10000 )
-            var req = 'M-SEARCH * HTTP/1.1\r\n' +
-                'HOST: ' + this.multicast + ':' + this.port + '\r\n' +
-                'MAN: "ssdp:discover"\r\n' +
-                'MX: 1\r\n' +
-                'ST: ' + this.searchdevice + '\r\n' +
-                '\r\n'
-            var state = {req:req, opts:opts}
+            setTimeout( this.stopsearch.bind(this), 2000 )
+            var state = {opts:opts}
             chrome.sockets.udp.create(function(sockInfo) {
                 state.sockInfo = sockInfo
                 this.sockMap[sockInfo.socketId] = state
@@ -141,7 +270,7 @@
                     if (result < 0) {
                         this.error({error:'ttl',code:result})
                     } else {
-                        chrome.sockets.udp.bind(state.sockInfo.socketId, '0.0.0.0', this.port, this.onbound.bind(this,state))
+                        chrome.sockets.udp.bind(state.sockInfo.socketId, '0.0.0.0', 0, this.onbound.bind(this,state))
                     }
                 }.bind(this))
             }.bind(this))
@@ -151,6 +280,10 @@
                 this.error({error:'bind error',code:result})
                 return
             }
+            chrome.sockets.udp.getInfo(state.sockInfo.socketId, this.onInfo.bind(this,state))
+        },
+        onInfo: function(state, info) {
+            this.boundPort = info.localPort
             console.clog(L.SSDP,'bound')
             chrome.sockets.udp.joinGroup(state.sockInfo.socketId, this.multicast, this.onjoined.bind(this,state))
         },
@@ -159,8 +292,15 @@
                 this.error({error:'join multicast',code:result})
                 return
             }
-            chrome.sockets.udp.send(state.sockInfo.socketId, new TextEncoder('utf-8').encode(state.req).buffer, this.multicast, this.port, this.onsend.bind(this))
-            console.clog(L.SSDP, 'sending to',this.multicast,this.port)
+            var req = 'M-SEARCH * HTTP/1.1\r\n' +
+                'HOST: ' + this.multicast + ':' + this.ssdpPort + '\r\n' +
+                'MAN: "ssdp:discover"\r\n' +
+                'MX: 1\r\n' +
+                'ST: ' + this.searchdevice + '\r\n' +
+                '\r\n'
+
+            chrome.sockets.udp.send(state.sockInfo.socketId, new TextEncoder('utf-8').encode(req).buffer, this.multicast, this.ssdpPort, this.onsend.bind(this))
+            console.clog(L.SSDP, 'sending to',this.multicast,this.ssdpPort)
         },
         onsend: function(result) {
             console.clog(L.SSDP, 'sent result',result)
